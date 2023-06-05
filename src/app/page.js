a0340e65b3aa9f74c7b3a09ae73239be97124acd
hyerani <hran9404@naver.com>
2023-06-05 17:15:06 +0900

    server side rendering

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,22 @@
 "use client";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+async function getData() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return results;
+}
+
+export default async function Home() {
+  const data = await getData();
 
   return (
     <div className="container">
       <title>Home | Next Movies</title>
-      {!movies && <h4>Loading..</h4>}
-      {movies?.map((movie) => (
+
+      {data?.map((movie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -26,6 +26,9 @@ export default function Home() {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
